cc0849b32263b839e6a95c9d26588d0b08432084
shinmyoungha <audgk0309@naver.com>
2025-05-01 12:28:17 +0900

    댓글 비동기 완료

---
+++ boardProject-boot/src/main/java/edu/kh/project/board/controller/CommentController.java
@@ -0,0 +1,69 @@
+package edu.kh.project.board.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.kh.project.board.model.dto.Comment;
+import edu.kh.project.board.model.service.CommentService;
+
+/* @RestController (REST API 구축을 위해서 사용하는 컨트롤러용 어노테이션)
+ * 
+ * ==> @Controller (요청/응답 제어 역할 명시 + bean 으로 등록)
+ *   + @ResponseBody (응답 본문으로 응답 데이터 자체를 반환)
+ *  
+ *  -> 모든 요청에 대한 응답을 응답 본문으로 반환하는 컨트롤러
+ *     (비동기 요청에 대한 컨트롤러)
+ * */
+
+//@Controller
+@RestController
+@RequestMapping("comments")
+public class CommentController {
+	
+	@Autowired
+	private CommentService service;
+	
+	/** 댓글 목록 조회
+	 * @param boardNo
+	 * @return
+	 */
+	@GetMapping("")
+	public List<Comment> select(@RequestParam("boardNo") int boardNo) {
+		// List<Comment> (Java 의 자료형 List)
+		// HttpMessageConverter 가 
+		// List -> JSON(문자열) 로 변환해서 응답 -> JS
+		return service.select(boardNo);
+	}
+	
+	/** 댓글/답글 등록
+	 * @return
+	 */
+	@PostMapping("")
+	public int insert(@RequestBody Comment comment) {
+		return service.insert(comment);
+	}
+	
+	@DeleteMapping("")
+	public int delete(@RequestBody int commentNo) {
+		return service.delete(commentNo);
+	}
+
+	/** 댓글 수정
+	 * @param comment
+	 * @return
+	 */
+	@PutMapping("")
+	public int update(@RequestBody Comment comment) {
+		return service.update(comment);
+	}
+}
